72058dc1f7bae83c79a29abfe57dc756c5226cbe
Laurence Cullen <laurencesimoncullen@gmail.com>
2023-05-28 14:57:40 +0100

    adding explosion and deletion logic

--- body.gd
+++ body.gd
@@ -1,6 +1,9 @@
 extends RigidBody2D
 
+
 var boom = preload("res://boom_1.tscn")
+
+var is_slung = get_meta("is_slung", null)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,7 +15,34 @@ func _process(delta):
 	pass
 	
 
+func delete_faster_planet(planet1, planet2):
+	if planet1.linear_velocity.length() > planet2.linear_velocity.length():
+		planet1.queue_free()
+	else:
+		planet2.queue_free()
+	
+func animate_explosion(body):
+	var boom_instance = boom.instantiate()
+	get_parent().add_child(boom_instance)
+	boom_instance.position = body.position
 
 func _on_body_entered(body):
-	var boom_instance = boom.instantiate()
-	add_child(boom_instance)
+	var other_body = get_colliding_bodies()[0]
+
+	animate_explosion(body)
+		# If we hit a star delete ourselves	
+	if other_body.name == 'Star':
+		queue_free()
+		return
+		
+	# If neither planet is slung delete the slower one
+	if not is_slung and not other_body.get_meta('is_slung'):
+		delete_faster_planet(body, other_body)
+	
+	# If both planets are slung delete the slower one
+	if is_slung and other_body.get_meta('is_slung'):
+		delete_faster_planet(body, other_body)
+	
+	# If the other planet is slung delete it
+	elif other_body.get_meta('is_slung'):
+		other_body.queue_free()
